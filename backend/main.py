ad6aaa6888d33405398f5a27f3ae01cf0b052a8e
David Kral <dockral@gmail.com>
2023-09-20 21:33:19 -0700

    Added Tailwind

--- backend/main.py
+++ backend/main.py
@@ -14,12 +14,12 @@ from routes.users import router as UserRouter
 from routes.clinics import router as ClinicRouter
 from routes.packs import router as PackRouter
 from routes.instruments import router as InstrumentRouter
-# from routes.procedures import router as ProcedureRouter
+from routes.procedures import router as ProcedureRouter
 
 # *** ROUTES *** #
-app.include_router(UserRouter)
-app.include_router(ClinicRouter)
-app.include_router(PackRouter)
-app.include_router(InstrumentRouter)
-# app.include_router(ProcedureRouter)
+app.include_router(UserRouter,prefix="/api", tags=["Users"])
+app.include_router(ClinicRouter, prefix="/api", tags=["Clinics"])
+app.include_router(PackRouter, prefix="/api", tags=["Packs"])
+app.include_router(InstrumentRouter, prefix="/api", tags=["Instruments"])
+app.include_router(ProcedureRouter, prefix="/api", tags=["Procedures"])
 
