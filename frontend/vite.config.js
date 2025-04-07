c23e87111ef480932c13c4c58fe08c4b8535149f
David Kral <dockral@gmail.com>
2025-04-07 07:24:50 -0700

    Vite config edit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  server: {
-    host: 'localhost',
-    port: 3000,
-  },
-  define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL),
-  },
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      host: 'localhost',
+      port: 3000,
+    },
+    define: {
+      'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL),
+    },
+    plugins: [react()],
+  }
 })
